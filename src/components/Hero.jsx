543a41f8f9723f0997d9dde4d2017b949a28fea8
kohlisneha <snehakohli01@gmail.com>
2024-06-10 20:00:47 +0530

    init repo

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+const HeroSection = () =>{
+    return <main className="hero conatiner">
+        <div className="hero-content">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES </p>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button className="secondary-btn">Category</button>
+            </div>
+        </div>
+        <div className="hero-image">
+            <img src="/public/Images/shoe_image.png" alt="shoe image" />
+        </div>
+
+    </main>
+}
+export default HeroSection
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,21 @@
+const Navigation =()=>{
+
+    return (
+        <nav className="conatiner">
+            <div className="logo">
+            <img src="/public/Images/brand_logo.png" alt="logo "></img>
+
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contact</li>
+
+            </ul>
+            <button>login</button>
+        </nav>
+    )
+
+}
+export default Navigation;
